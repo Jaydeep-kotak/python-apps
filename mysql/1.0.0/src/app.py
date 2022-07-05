cc9724cc28d3644f6dc4bb4005dbb4a6e1f355b2
Jaydeep-kotak <103485309+Jaydeep-kotak@users.noreply.github.com>
2022-07-05 11:51:42 +0530

    Update app.py

--- mysql/1.0.0/src/app.py
+++ mysql/1.0.0/src/app.py
@@ -257,7 +257,7 @@ class MySQL(AppBase):
             cursor = conn.cursor()
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your seur name or password")
+                print("Something is wrong with your user name or password")
                 return "Something is wrong with your user name or password"
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
                 print("Database does not exist")
